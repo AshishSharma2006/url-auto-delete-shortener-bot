1b8dc97f4e9101bf08204052100fc0ae3b46c449
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2023-01-19 21:39:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,8 +68,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'shorturllink.in')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '849c92eaea9a05316c89d8461ff8465fffa2cf4e')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'False')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '')
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
